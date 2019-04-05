b807d6d3e146e80bdcaa5a4552ca15bbcdde71db
Takumu Uyama <t_uyama@r.recruit.co.jp>
2019-04-05 14:25:02 +0900

    update: http通信を許可

---
+++ weather/UseCase/FetchWeatherUseCaseImpl.swift
@@ -0,0 +1,15 @@
+//
+//  FetchWeatherUseCase.swift
+//  weather
+//
+//  Created by 宇山拓夢 on 2019/04/05.
+//  Copyright © 2019 宇山拓夢. All rights reserved.
+//
+
+import Foundation
+
+class FetchWeatherUseCase {
+    func fetch(areaCode: Int) {
+        val request = URLRequest(url ")
+    }
+}
