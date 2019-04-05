5c6d25ce1ab1185c6dc52445bab7205101cfe715
Takumu Uyama <t_uyama@r.recruit.co.jp>
2019-04-05 16:51:18 +0900

    add: パーシャルビュ作成

---
+++ weather/Presenters/HomePresenter.swift
@@ -0,0 +1,9 @@
+//
+//  HomePresenter.swift
+//  weather
+//
+//  Created by 宇山拓夢 on 2019/04/05.
+//  Copyright © 2019 宇山拓夢. All rights reserved.
+//
+
+import Foundation
---
+++ weather/Views/Xibs/WeatherForecastListItem.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherForecastListItem.swift
+//  weather
+//
+//  Created by 宇山拓夢 on 2019/04/05.
+//  Copyright © 2019 宇山拓夢. All rights reserved.
+//
+
+import UIKit
+
+class WeatherForecastListItem: UITableViewCell {
+    @IBOutlet var labelDate: UILabel!
+    @IBOutlet var labelTelop: UILabel!
+    @IBOutlet var labelMinTemperature: UILabel!
+    @IBOutlet var labelMaxTemperature: UILabel!
+}
