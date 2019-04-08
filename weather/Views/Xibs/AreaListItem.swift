1359c1daef734bd0b3095ae7adcf5f50c8f7b1a5
Takumu Uyama <t_uyama@r.recruit.co.jp>
2019-04-08 17:55:38 +0900

    add: Areaリストアイテム

---
+++ weather/Views/Xibs/AreaListItem.swift
@@ -0,0 +1,13 @@
+//
+//  AreaListItem.swift
+//  weather
+//
+//  Created by 宇山拓夢 on 2019/04/08.
+//  Copyright © 2019 宇山拓夢. All rights reserved.
+//
+
+import UIKit
+
+class AreaListItem: UITableViewCell {
+    @IBOutlet var labelArea: UILabel!
+}
