94614dfa92288bda019ab3ec1532a9c3215f3764
Takumu Uyama <t_uyama@r.recruit.co.jp>
2019-04-08 17:38:06 +0900

    add: エリア選択ページ白紙

---
+++ weather/ViewControllers/AreaIndexViewController.swift
@@ -0,0 +1,12 @@
+//
+//  AreaIndexViewController.swift
+//  weather
+//
+//  Created by 宇山拓夢 on 2019/04/08.
+//  Copyright © 2019 宇山拓夢. All rights reserved.
+//
+
+import UIKit
+
+class AreaIndexViewController: UIViewController {
+}
